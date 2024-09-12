bcfdb58b2ea0cfaf6699ada38c9b5ac49389a3f9
isurucis <143800833+isurucis@users.noreply.github.com>
2024-09-12 13:28:36 +0530

    Update fmmquick.php
    add product service

--- controllers/front/fmmquick.php
+++ controllers/front/fmmquick.php
@@ -23,7 +23,7 @@
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
-
+require_once _PS_MODULE_DIR_ . 'quickproducttable/lib/ProductService.php';
 require_once _PS_MODULE_DIR_ . 'quickproducttable/lib/CSVReader.php';
 require_once _PS_MODULE_DIR_ . 'quickproducttable/lib/Csv.php';
 class QuickProductTableFmmQuickModuleFrontController extends ModuleFrontController
